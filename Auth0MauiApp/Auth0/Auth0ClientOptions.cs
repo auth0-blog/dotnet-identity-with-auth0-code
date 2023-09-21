2feecc487cd0f24cc01ffc9784a26226bfe8c7af
Andrea Chiarelli <25705235+andychiare@users.noreply.github.com>
2023-09-21 15:48:21 +0200

    Add Maui app and update Blazor Server

---
+++ Auth0MauiApp/Auth0/Auth0ClientOptions.cs
@@ -0,0 +1,24 @@
+namespace Auth0MauiApp.Auth0;
+
+public class Auth0ClientOptions
+{
+  public Auth0ClientOptions()
+  {
+    Scope = "openid";
+    RedirectUri = "myapp://callback";
+    Browser = new WebBrowserAuthenticator();
+    Audience = "";
+  }
+
+  public string Domain { get; set; }
+
+  public string ClientId { get; set; }
+
+  public string RedirectUri { get; set; }
+
+  public string Scope { get; set; }
+  
+  public string Audience { get; set; }
+
+  public IdentityModel.OidcClient.Browser.IBrowser Browser { get; set; }
+}
---
+++ Auth0MauiApp/Auth0/TokenHolder.cs
@@ -0,0 +1,6 @@
+namespace Auth0MauiApp.Auth0;
+
+public static class TokenHolder
+{
+  public static string AccessToken { get; set; }
+}
